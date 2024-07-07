26e57d91c2bf2a010e0f69536fb69ed2b4a0010e
TackTeam-Chare <chare.auppachitwan@gmail.com>
2024-07-08 01:04:21 +0700

    เเสดงเเผนที่ตามไอดีเเละเเผนที่หน้าหลัก

--- src/app/tourism/page.js
+++ src/app/tourism/page.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import TourismComponent from '@/components/tourism/page'
 import Layout from '@/components/common/layout';
+import MapFetch from '@/components/map/MapFetch';
 function TourismPage() {
   return (
     <div>
       <Layout>
       <TourismComponent/>
+      <MapFetch/>
       </Layout>
     </div>
   )
--- src/components/tourism/TourismDetails.js
+++ src/components/tourism/TourismDetails.js
@@ -1,5 +1,8 @@
 
 'use client';
+import React from 'react';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import Map_ID from '@/components/map/MapID';
 const TourismDetailsByIdComponent = ({ tourismData }) => {
   if (!tourismData) {
     return <p>Loading...</p>;
@@ -18,8 +21,21 @@ const TourismDetailsByIdComponent = ({ tourismData }) => {
           {/* Add more fields as needed */}
         </div>
       </div>
-    </div>
 
+      {/* Leaflet Map */}
+      <div className="mt-8">
+        <h2 className="text-xl font-semibold mb-4">Location on Map</h2>
+        <MapContainer center={[tourismData.latitude, tourismData.longitude]} zoom={15} style={{ height: '400px' }}>
+          <TileLayer
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+          <Marker position={[tourismData.latitude, tourismData.longitude]}>
+            <Popup>{tourismData.name}</Popup>
+          </Marker>
+        </MapContainer>
+      </div>
+      <Map_ID tourist_entities={tourismData[0]} />
+    </div>
   );
 };
 
