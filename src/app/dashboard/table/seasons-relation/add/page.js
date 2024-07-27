dd3874ce179d56e7fc58352dbcd17614fcd24872
TackTeam-Chare <chare.auppachitwan@gmail.com>
2024-07-27 14:57:01 +0700

    check insert ฤดูกาลซ้ำ

--- src/app/dashboard/table/seasons-relation/add/page.js
+++ src/app/dashboard/table/seasons-relation/add/page.js
@@ -48,6 +48,21 @@ const AddSeasonsRelationForm = () => {
         <h2 className="text-2xl font-bold mb-5 text-center text-indigo-600">Add Seasons Relation</h2>
         <form onSubmit={handleSubmit(onSubmit)} className="space-y-6">
           <div className="relative z-0 w-full mb-6 group">
+            <label htmlFor="tourism_entities_id" className="block text-sm font-medium text-gray-700">Tourism Entity :สถานที่</label>
+            <select
+              id="tourism_entities_id"
+              name="tourism_entities_id"
+              {...register('tourism_entities_id', { required: 'Tourism Entity is required' })}
+              className="block mt-1 w-full py-2 px-3 bg-white border border-gray-300 rounded-md shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+            >
+              <option value="">Select a tourism entity</option>
+              {touristEntities.map(entity => (
+                <option key={entity.id} value={entity.id}>{entity.name}</option>
+              ))}
+            </select>
+            {errors.tourism_entities_id && <p className="text-red-500 text-xs mt-1">{errors.tourism_entities_id.message}</p>}
+          </div>
+          <div className="relative z-0 w-full mb-6 group">
             <label htmlFor="season_id" className="block text-sm font-medium text-gray-700">Season</label>
             <select
               id="season_id"
@@ -63,21 +78,6 @@ const AddSeasonsRelationForm = () => {
               ))}
             </select>
             {errors.season_id && <p className="text-red-500 text-xs mt-1">{errors.season_id.message}</p>}
-          </div>
-          <div className="relative z-0 w-full mb-6 group">
-            <label htmlFor="tourism_entities_id" className="block text-sm font-medium text-gray-700">Tourism Entity</label>
-            <select
-              id="tourism_entities_id"
-              name="tourism_entities_id"
-              {...register('tourism_entities_id', { required: 'Tourism Entity is required' })}
-              className="block mt-1 w-full py-2 px-3 bg-white border border-gray-300 rounded-md shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
-            >
-              <option value="">Select a tourism entity</option>
-              {touristEntities.map(entity => (
-                <option key={entity.id} value={entity.id}>{entity.name}</option>
-              ))}
-            </select>
-            {errors.tourism_entities_id && <p className="text-red-500 text-xs mt-1">{errors.tourism_entities_id.message}</p>}
           </div>
           <button
             type="submit"
